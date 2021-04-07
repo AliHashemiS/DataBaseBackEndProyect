eea60f4c3281011d06936d78262827e50cd2163b
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2021-04-07 00:07:36 -0600

    Update test_controller.ex

--- lib/proyecto_bases_api_web/controllers/test_controller.ex
+++ lib/proyecto_bases_api_web/controllers/test_controller.ex
@@ -1,12 +1,15 @@
 defmodule ProyectoBasesApiWeb.TestController do
   use ProyectoBasesApiWeb, :controller
 
-  def index(conn, _params) do
-    {:ok, pid} = Postgrex.start_link(hostname: "localhost", username: "root", password: "root", database: "battery_steward_api_development")
+  def index(conn, %{"server" => server, "port" => port, "username" => username, "password" => password, "database" => database} = params) do
+    IO.inspect (params[:server])
+    {:ok, pid} = Postgrex.start_link(hostname: server, username: username, password: password, database: database)
 
-    json(conn, %{
-      name: "Kendall",
-      last_name: "W22222222alsh"
-      })
+    result = Postgrex.query!(pid, "SELECT  table_schema,table_name from information_schema.tables where table_schema not in ('pg_catalog','information_schema') and table_type='BASE TABLE'", [])
+    %Postgrex.Result{}
+    result2 = Postgrex.query!(pid, "SELECT column_name,data_type from information_schema.columns where table_name='user_account'", [])
+    %Postgrex.Result{}
+
+    json(conn, result.rows)
   end
 end
